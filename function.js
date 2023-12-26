6d9a79dfbfee9d1aca44727b26cf998981b8c4de
MatiCBL <brzoza.mathew@gmail.com>
2023-12-26 20:15:49 +0000

    'created-get-completion-function-to-communicate-to-openAI-to-use-provided-messages.It-specifies-that-the-model-can-use-a-function-named-calculate-to-evaluate-math-expresssions'

--- function.js
+++ function.js
@@ -14,3 +14,30 @@ const functions = {
     return math.evaluate(expression);
   },
 };
+
+const getCompletion = async (messages) => {
+  return openai.chat.completions.create({
+    model: "gpt-3.5-turbo-0613",
+    messages,
+    functions: [
+      {
+        name: "calculate",
+        description: "Run a math expression",
+        parameters: {
+          type: "object",
+          properties: {
+            expression: {
+              type: "string",
+              description:
+                'The math expression to evaluate like "2 * 3 + (21 / 2 ) ^ 2"',
+            },
+          },
+          required: ["expression"],
+        },
+      },
+    ],
+    temperature: 0,
+  });
+
+  return response;
+};
